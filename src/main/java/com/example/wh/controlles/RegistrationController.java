2163d7fc17228d3f930cc6dd6269a029b514a7c4
Железнякович Максим <maxssmail572@gmail.com>
2023-04-30 15:54:13 +0300

    5-7 lessons

--- src/main/java/com/example/wh/controlles/RegistrationController.java
+++ src/main/java/com/example/wh/controlles/RegistrationController.java
@@ -15,21 +15,25 @@ import java.util.Map;
 public class RegistrationController {
     @Autowired
     private UserRepository userRepository;
+
     @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
+
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model) {
         User userFromDb = userRepository.findByUsername(user.getUsername());
+
         if (userFromDb != null) {
-            model.put("message", "User exist");
+            model.put("message", "User exists!");
             return "registration";
         }
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepository.save(user);
+
         return "redirect:/login";
     }
 }
