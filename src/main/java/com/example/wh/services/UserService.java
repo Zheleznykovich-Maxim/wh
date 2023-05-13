45b7e7528d76de3cca606fc3126e88a80e95ced4
Железнякович Максим <maxssmail572@gmail.com>
2023-05-13 22:51:37 +0300

    fixed

--- src/main/java/com/example/wh/services/UserService.java
+++ src/main/java/com/example/wh/services/UserService.java
@@ -89,8 +89,8 @@ public class UserService implements UserDetailsService {
     public void updateProfile(User user, String password, String email) {
         String userEmail = user.getEmail();
 
-        boolean isEmailChanged = (email != null && email.equals(userEmail))
-                || (userEmail != null && userEmail.equals(email));
+        boolean isEmailChanged = (email != null && !email.equals(userEmail))
+                || (userEmail != null && !userEmail.equals(email));
         if (isEmailChanged) {
             user.setEmail(email);
 
