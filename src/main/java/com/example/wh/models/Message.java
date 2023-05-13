cde8f21f7aebfcbeed8661a73cb8a693496a3647
Железнякович Максим <maxssmail572@gmail.com>
2023-05-13 22:47:44 +0300

    ???

--- src/main/java/com/example/wh/models/Message.java
+++ src/main/java/com/example/wh/models/Message.java
@@ -1,6 +1,9 @@
 package com.example.wh.models;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
@@ -8,14 +11,16 @@ public class Message {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 2048, message = "Message too long (more than 2kB)")
     private String text;
+    @Length(max = 255, message = "Message too long (more than 255)")
     private String tag;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
 
     private String filename;
-
 
     public Message() {
     }
